8ae1cad8ae3048cfd80bbef7890a3c9aa38c1006
flcwalker <41645069+flcwalker@users.noreply.github.com>
2023-07-18 11:35:20 -0500

    Update script.js (Último)

--- script.js
+++ script.js
@@ -1,51 +1,47 @@
-// scripts.js
-const botonAgregar = document.querySelector(".boton-agregar");//Se selecciona el elemento del documento por medio de su clase y se asigna a la constante.
-const inputRecetas = document.getElementById("recetas-input");//Se selecciona el elemento del documento por medio de su Id y se asigna a la constante.
-const listaIngredientes = document.getElementById("lista-ingredientes");//Se selecciona el elemento del documento por medio de su ID y e asigna a la constante.
+// Arrays para almacenar ingredientes y recetas
+let ingredientes = [];
+let recetas = [];
 
-botonAgregar.addEventListener('click', () => {//Se e agregar un detector de evento a la contante botonAgregar de tipo 'click'.
-  const nuevoIngrediente = inputRecetas.value;//Se asigna el valor ingresado en inputRecetas a la contante nuevoIngrediente.  
-  if (nuevoIngrediente !== '') {//Si el valor almacenado dentro de la variable, es diferentes a un valor vacío.
-    const li = document.createElement('li');//Cree un elemento 'li' y almcenelo en la contante li.
-    li.textContent = nuevoIngrediente;//Se le asigna el valor de texto almacenado en la variable nuevoIngrediente a el teto de la variable li.
-    listaIngredientes.appendChild(li);//Se le adjunta el valor de la variable li como elemento hijo a la variable listaIngredientes.
-    inputRecetas.value = '';//Una ve adjuntado la variable li, e formatea el input, para permitir el ingreso de otro ingrediente.
+function agregarIngrediente() {
+  const ingredienteInput = document.getElementById("ingredienteInput");
+  const ingrediente = ingredienteInput.value.trim();
+
+  if (ingrediente !== "") {
+    ingredientes.push(ingrediente);
+    actualizarListaIngredientes();
+    ingredienteInput.value = "";
   }
-});
+}
 
-const botonReceta = document.querySelector('.boton-recetas');
+function agregarReceta() {
+  const recetaInput = document.getElementById("recetaInput");
+  const receta = recetaInput.value.trim();
 
-botonReceta.addEventListener('click', () => {
-    const ingredientes = obtenerIngredientes();
-    if (ingredientes.length > 0) {
-    const nuevaReceta = generarReceta(ingredientes);
-    }else {
-      alert("Debe ingresar almenos un ingrediente dentro de la lista, antes de generar alguna receta.");
-    }const botonRecetas = document.querySelector('.boton-recetas');
+  if (receta !== "") {
+    recetas.push(receta);
+    actualizarListaRecetas();
+    recetaInput.value = "";
+  }
+}
 
-    botonRecetas.addEventListener('click', () => {
-      const ingredientes = obtenerIngredientes();
-      if (ingredientes.length > 0) {
-        const nuevaReceta = generarReceta(ingredientes);
-        // Aquí puedes hacer lo que quieras con la nueva receta, como mostrarla en una alerta o en algún elemento del DOM.
-      } else {
-        alert("Ingrese al menos un ingrediente en la lista antes de generar una receta.");
-      }
-    });
-    
-    function generarReceta(ingredientes) {
-      // Aquí puedes implementar la lógica para generar la receta con los ingredientes.
-      // Por ejemplo, puedes combinar los ingredientes y crear una cadena que represente la receta.
-      // Luego, puedes devolver esa cadena o cualquier otro formato que desees para la receta.
-      // Aquí un ejemplo simple de cómo podrías combinar los ingredientes en una receta:
-      const receta = "Receta: " + ingredientes.join(", ");
-      return receta;
-    }
-    
-    
-    function generarReceta(ingrediente) {
+function actualizarListaIngredientes() {
+  const listaIngredientes = document.getElementById("listaIngredientes");
+  listaIngredientes.innerHTML = "";
 
-      const receta = "Receta" + ingredientes.join(", ");
-      return receta;
-    }
-})
+  ingredientes.forEach((ingrediente) => {
+    const li = document.createElement("li");
+    li.textContent = ingrediente;
+    listaIngredientes.appendChild(li);
+  });
+}
+
+function actualizarListaRecetas() {
+  const listaRecetas = document.getElementById("listaRecetas");
+  listaRecetas.innerHTML = "";
+
+  recetas.forEach((receta) => {
+    const li = document.createElement("li");
+    li.textContent = receta;
+    listaRecetas.appendChild(li);
+  });
+}
